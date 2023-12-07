85e2cc63d97ce5593c94eecadc85fb489e097e62
jorgito <jdasilv8@educacion.navarra.es>
2023-12-07 16:15:39 +0100

    a

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,1 +1,10 @@
-file = open('saludo.txt', 'w')
+numero = (int(input("Introduce un número: ")))
+def ficherotabla (n):
+    tabla = []
+    for i in range(0,11):
+        valor = n * i
+        file = open("Tabla del", 'w')
+        file.write (n, "*" , i, "=", valor)
+    return
+
+ficherotabla (numero)
