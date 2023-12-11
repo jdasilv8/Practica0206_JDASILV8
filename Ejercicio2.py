43087c10c5bdb9c2535cafd558135eb78467b61c
jorgito <jdasilv8@educacion.navarra.es>
2023-12-11 20:08:14 +0100

    a

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,4 +1,11 @@
+import os
 numero = (int(input("Introduce un número del 1 al 10: ")))
 def leertabla (n):
-    file = open("tabla-" + str(n) + ".txt", 'r')
-    print (file.read())
+    existe = os.path.isfile ("tabla-" + str(n) + ".txt")
+    if existe == True:
+        file = open("tabla-" + str(n) + ".txt", 'r')
+        print (file.read())
+    else:
+        print ("El archivo con el nombre tabla", n ,"no existe")
+
+leertabla (numero)
