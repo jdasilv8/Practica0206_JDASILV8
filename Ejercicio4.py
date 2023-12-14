1bb7ee94ef0782a619cf96afcf2237ce7f955701
jorgito <jdasilv8@educacion.navarra.es>
2023-12-14 15:50:21 +0100

    a

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,10 +1,12 @@
-import urllib.request
-url = "http://textfiles.com/adventure/aencounter.txt"
 def leerURL (url):
+    import urllib.request
     file = urllib.request.urlopen(url)
     texto = file.read()
     palabras = texto.split()
     numero = len(palabras)
     print (numero)
 
+
+
+url = "http://textfiles.com/adventure/aencounter.txt"
 leerURL (url)
