683a1126e2603056b1107768d91867d64965bea0
jorgito <jdasilv8@educacion.navarra.es>
2023-12-14 16:45:23 +0100

    Update Ejercicio5.py

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,7 +1,11 @@
+ppais = input("Indica el pais: ")
 file = open ('sdg_08_10.tsv', 'r')
 texto = file.read()
-texto = str(texto)
 lineas = texto.split("\n")
-print (lineas[1])
-for i in lineas:
-    pais = lineas[i+1]
+for i in range (1, len(lineas)):
+    pais = lineas[i]
+    pais = pais.split(",")
+    linea = pais[2]
+    contlinea = linea.split()
+    if ppais.upper() == contlinea[0]:
+        print (contlinea)
